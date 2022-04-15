c7c84a3ebeb7ef3226e0db6255ee5901f0f5c032
Dlanor91 <limaronald926@gmail.com>
2022-04-15 17:50:53 -0300

    FindByIdMedias_Ronald

--- Repositorios/RepositorioPlantaADO.cs
+++ Repositorios/RepositorioPlantaADO.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Data;
 using Dominio.EntidadesVivero;
 using Dominio.InterfacesRepositorio;
+using Microsoft.Data.SqlClient;
 
 namespace Repositorios
 {
@@ -45,9 +46,45 @@ namespace Repositorios
         }
 
         public Planta FindById(int id)
-        {
-            throw new NotImplementedException();
+        { 
+            SqlConnection con = Conexion.ObtenerConexion();
+
+            string sql = "select pl.id, pl.nombreCientifico, pl.descripcionPlanta, pl.alturaMax, pl.foto, ta.tipoAmbiente, pl.frecuenciaRiego, pl.temperatura, tp.nombre,il.tipoIluminacion,pl.nombreVulgares from Planta pl " +
+                         "left join TipoAmbiente ta on pl.tipoAmbiente = ta.id " +
+                         "left join TipoPlanta tp on pl.tipoPlanta = tp.id " +
+                         "left join Iluminacion il on pl.tipoIluminacion = il.id " +
+                         "where pl.id = @id; ";
+            SqlCommand com = new SqlCommand(sql, con);
+            com.Parameters.AddWithValue("@id", id);
+
+            try
+            {
+                Conexion.AbrirConexion(con);
+                SqlDataReader reader = com.ExecuteReader();
+
+                if (reader.Read())
+                {                    
+                    Planta pl = CrearPlanta(reader);
+                    pl.tipoAmbiente = CrearTipoAmbiente(reader);
+                    pl.tipoPlanta = CrearTipoPlanta(reader);
+                    pl.tipoIlumincacion = CrearIluminacion(reader);
+                }
+
+                Conexion.CerrarConexion(con);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+
+                Conexion.CerrarYDisposeConexion(con);
+            }
+
+            return plantaBuscada;
         }
+             
 
         public bool Remove(int id)
         {
