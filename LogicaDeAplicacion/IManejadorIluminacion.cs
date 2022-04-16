bf12491bb75a253552aea00e655ff0c1fde96c5c
Dlanor91 <limaronald926@gmail.com>
2022-04-16 12:24:34 -0300

    Manejador Iluminacion Implementado

---
+++ LogicaDeAplicacion/IManejadorIluminacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dominio.InterfacesRepositorio;
+using Dominio.EntidadesVivero;
+
+namespace LogicaDeAplicacion
+{
+    public interface IManejadorIluminacion
+    {
+        public IEnumerable<Iluminacion> MostrarTodasIluminaciones();
+        bool AgregarTipoPlanta(Iluminacion il);
+    }
+}
