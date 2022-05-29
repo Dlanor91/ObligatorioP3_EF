1cd2eee9da2d2199c74e63b9dd3d3fc784c59b49
Mauricio Fernandez <mfernandez657@gmail.com>
2022-05-29 18:32:15 -0300

    updated RepositorioTipoAmbienteEF.cs & RepositorioTipoPlantaEF.cs

--- Repositorios/RepositorioTipoPlantaEF.cs
+++ Repositorios/RepositorioTipoPlantaEF.cs
@@ -16,42 +16,16 @@ namespace Repositorios
     public class RepositorioTipoPlantaEF : IRepositorioTipoPlanta   
 
     {
+        public ViveroContext Contexto { get; set; }
+
+        public RepositorioTipoPlantaEF(ViveroContext cont)
+        {
+            Contexto = cont;
+        }
         public bool Add(TipoPlanta obj)
         {
-            bool listo = false;
-
-            SqlConnection con = Conexion.ObtenerConexion();
-            string sql = "INSERT INTO TipoPlanta VALUES(@nombre,@desc);";
-            SqlCommand com = new SqlCommand(sql, con);
-
-            com.Parameters.AddWithValue("@nombre",obj.nombre);
-            com.Parameters.AddWithValue("@desc", obj.descripcionTipo);
-
-            SqlTransaction transaccion = null;
-
-            try
-            {
-                Conexion.AbrirConexion(con);
-                transaccion = con.BeginTransaction();
-                com.Transaction = transaccion;
-                int filasAfectadas = com.ExecuteNonQuery();
-                listo = filasAfectadas == 1;
-                transaccion.Commit();
-                Conexion.CerrarConexion(con);
-
-            }
-            catch
-            {
-                if (transaccion != null) transaccion.Rollback();
-                listo = false;
-               
-            }
-            finally 
-            {
-                Conexion.CerrarYDisposeConexion(con);
-            }
-
-            return listo;
+            Contexto.TipoPlantas.Add(obj);
+            return Contexto.SaveChanges() >=1;
         }
         
         public TipoPlanta buscarTipoPlanta(string nombreTipo)
@@ -131,144 +105,25 @@ namespace Repositorios
 
         public IEnumerable<TipoPlanta> FindAll()
         {
-            List<TipoPlanta> tipoPlantas = new List<TipoPlanta>();
-
-            SqlConnection con = Conexion.ObtenerConexion();
-
-            string sql = "SELECT * FROM TipoPlanta;";
-            SqlCommand com = new SqlCommand(sql, con);
-
-            try
-            {
-                Conexion.AbrirConexion(con);
-                SqlDataReader reader = com.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    TipoPlanta tp = new TipoPlanta()
-                    {
-                        id = reader.GetInt32(0),
-                        nombre = reader.GetString(1),
-                        descripcionTipo = reader.GetString(2)                        
-                    };
-                    tipoPlantas.Add(tp);   
-                }
-
-                Conexion.CerrarConexion(con);
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                Conexion.CerrarYDisposeConexion(con);
-            }
-
-            return tipoPlantas;
+            return Contexto.TipoPlantas.ToList();
         }
 
         public TipoPlanta FindById(int id)
         {
-            TipoPlanta tipoPlantaBuscada = null;
-
-            SqlConnection con = Conexion.ObtenerConexion();
-
-            string sql = "SELECT * FROM TipoPlanta WHERE id=@id;";
-            SqlCommand com = new SqlCommand(sql, con);
-            com.Parameters.AddWithValue("@id", id);
-
-            try
-            {
-                Conexion.AbrirConexion(con);
-                SqlDataReader reader = com.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    tipoPlantaBuscada = new TipoPlanta()
-                    {                        
-                        id = reader.GetInt32(0),
-                        nombre = reader.GetString(1),
-                        descripcionTipo = reader.GetString(2)                      
-                    };
-                }
-
-                Conexion.CerrarConexion(con);
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                Conexion.CerrarYDisposeConexion(con);
-            }
-
-            return tipoPlantaBuscada;
+            return Contexto.TipoPlantas.Find(id);
         }
         
 
         public bool Remove(int id)
         {
-            bool ok = false;
-
-            SqlConnection con = Conexion.ObtenerConexion();
-
-            string sql = "DELETE FROM TipoPlanta WHERE Id=@id;";
-            SqlCommand com = new SqlCommand(sql, con);
-            com.Parameters.AddWithValue("@id", id);
-
-            try
-            {
-                Conexion.AbrirConexion(con);
-                int filasAfectadas = com.ExecuteNonQuery();
-                ok = filasAfectadas == 1;
-                Conexion.CerrarConexion(con);
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                Conexion.CerrarYDisposeConexion(con);
-            }
-
-            return ok;
+            Contexto.TipoPlantas.Remove(new TipoPlanta() { Id = id});
+            return Contexto.SaveChanges() >=1;
         }
 
         public bool Update(TipoPlanta obj)
         {
-            bool tipoPlantaUpdate = false;
-            
-            SqlConnection con = Conexion.ObtenerConexion();
-            
-            string sql = "UPDATE TipoPlanta SET nombre=@nom, descripcionTipo=@desc WHERE id=@id;";
-            SqlCommand com = new SqlCommand(sql, con);
-
-            com.Parameters.AddWithValue("@id", obj.id);
-            com.Parameters.AddWithValue("@nom", obj.nombre);
-            com.Parameters.AddWithValue("@desc", obj.descripcionTipo);
-     
-            
-            try
-            {
-                Conexion.AbrirConexion(con);
-                int filasAfectadas = com.ExecuteNonQuery();
-                tipoPlantaUpdate = filasAfectadas == 1;
-                Conexion.CerrarConexion(con);
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                Conexion.CerrarYDisposeConexion(con);
-            }
-
-            return tipoPlantaUpdate;
-            
+            Contexto.TipoPlantas.Update(obj);
+            return Contexto.SaveChanges()>=1;
         }
     }
 }
