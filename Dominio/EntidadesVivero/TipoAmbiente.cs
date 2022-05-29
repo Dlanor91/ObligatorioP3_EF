403fb1a8653f6a64e73db675f45ff0ddf43e4e05
Mauricio Fernandez <mfernandez657@gmail.com>
2022-05-29 17:50:02 -0300

    updated Plaza.cs & TipoAmbiente.cs

--- Dominio/EntidadesVivero/TipoAmbiente.cs
+++ Dominio/EntidadesVivero/TipoAmbiente.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dominio.EntidadesVivero
 {
+    [Table("TipoAmbiente")]
     public class TipoAmbiente
     {
-        public int id { get; set; }
-
-        public string tipoAmbiente { get; set; }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string  Ambiente { get; set; }
     }
 }
