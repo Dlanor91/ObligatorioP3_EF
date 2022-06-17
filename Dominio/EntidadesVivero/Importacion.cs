e98270fb04497267304b8da90c2c8228f9cfb29d
Mauricio Fernandez <MF274679@fi365.ort.edu.uy>
2022-06-16 23:14:10 -0300

    validar importacion

--- Dominio/EntidadesVivero/Importacion.cs
+++ Dominio/EntidadesVivero/Importacion.cs
@@ -23,7 +23,7 @@ namespace Dominio.EntidadesVivero
         {
             bool ret = false;
 
-            if (Fecha != null)
+            if (Fecha != null && DescripcionSanitaria != null && OrigenAmericaSur != null)
             {
                 if (Item != null)
                 {
